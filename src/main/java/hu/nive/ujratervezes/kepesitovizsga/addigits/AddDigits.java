083a7196673e37f5c3559ad85ca264fb80b120ba
wakesz87 <kisszab87@gmail.com>
2021-04-29 10:51:10 +0200

    Add Digit

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -2,4 +2,24 @@ package hu.nive.ujratervezes.kepesitovizsga.addigits;
 
 public class AddDigits {
 
+    private int n;
+
+    public int addDigits(String input){
+        if (input == null || input.isBlank()){
+            return -1;
+        }
+        int count = 0;
+        char[] chars= input.toCharArray();
+        for (char c : chars){
+            if(Character.isDigit(c)){
+                count+=Integer.parseInt(Character.toString(c));
+            }
+        }
+
+        return count;
+
+
+
+    }
+
 }
